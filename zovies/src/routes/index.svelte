1b534d57d03745e8af6ea60cbc7b4599eb43c8f8
Zed <emailzed03@gmail.com>
2022-05-11 12:00:32 +1000

    added card grid

--- zovies/src/routes/index.svelte
+++ zovies/src/routes/index.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Button, { Label, Icon } from '@smui/button';
+	import Card, {Media} from '@smui/card';
+
 	import '@videojs/themes/dist/city/index.css';
+	import MovieCard from '../components/MovieCard.svelte';
+	import CardGrid from '../components/CardGrid.svelte';
 	
 	let clicked = 0;
 
@@ -29,13 +33,22 @@
 	{/if}
 </p>
 
-<video id="movie-player" class="video-js vjs-deafult-skin" controls preload="auto" width="640" height="264">
-	<source src="file:///Users/zed/Projects/Zovies-UI/zovies/example.mp4" type="video/mp4" />
-</video>
+
+<CardGrid/>
+
+<!-- <video id="movie-player" class="video-js vjs-deafult-skin" controls preload="auto" width="640" height="264">
+	<source src="./example.mp4" type="video/mp4" />
+</video> -->
+
+
 
 
 <style>
 	.grayed {
 		opacity: 0.6;
 	}
+
+	* :global(.card-media-square) {
+    	background-image: url(https://place-hold.it/320x320?text=square&fontsize=23);
+  	}
 </style>
