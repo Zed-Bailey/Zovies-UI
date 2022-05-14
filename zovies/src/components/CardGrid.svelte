d266adcabe851ea94c7c40058d17b8122b58d3b3
Zed <emailzed03@gmail.com>
2022-05-14 16:33:14 +1000

    fetch data from api in card grid and pass down to card

--- zovies/src/components/CardGrid.svelte
+++ zovies/src/components/CardGrid.svelte
@@ -1,32 +1,59 @@
 <script lang="ts">
     import MovieCard from './MovieCard.svelte';
+    import LayoutGrid from '@smui/layout-grid';
+import Cell from '@smui/layout-grid/src/Cell.svelte';
+
+    export let data: [];
 </script>
 
 
 <div class="flex-grid" >
-    {#each Array(10) as _unused, _i}
+    {#each data as _movie, _i}
     <div class="col">
-        <MovieCard movieID="{_i.toString()}" title="{"movie title: " + _i}"/>
+        <MovieCard movieID="{_movie['movieId']}" title="{_movie['name']}" coverImageUrl={_movie['cover']}/>
     </div>
     {/each}
 </div>
 
+<!-- <LayoutGrid>
+    {#each data as _movie, _i}
+    <Cell >
+        <div class="gridCell">
+            <MovieCard movieID="{_movie['movieId']}" title="{_movie['name']}" coverImageUrl={_movie['cover']}/>
+        </div>
+        
+    </Cell>
+    {/each}
+
+</LayoutGrid> -->
 
 <style>
+
+
+
+
+
+
     .flex-grid {
         display: flex;
+        width: 100%;
+        justify-content: flex-start;
+
         flex-wrap: wrap;
-        justify-content: start;
+        /* display: flex;
+        
+        flex-direction: row;
+        /* justify-content: flex-start; */
         /* adjust to center grid */
         /* width: 80%; */
-        height: 100%;
+        /* height: 100%; */
     }
 
     .col {
         min-width: 300px;
         height: 450px;
         margin: 20px;
-        justify-content: start;
+        /* justify-content: start; */
     }
     
     @media (max-width: 499px) {
--- zovies/src/components/MovieCard.svelte
+++ zovies/src/components/MovieCard.svelte
@@ -4,7 +4,8 @@
     // the movies title
     export let title: string;
     // the movies image url, default to placeholder if nothing is passed in
-    export let coverImageUrl:string = "https://place-hold.it/300x450?text=square&fontsize=23";
+    // while fetching the cover image, if it returns 404 then use the placehold-it and make the text the movie name
+    export let coverImageUrl:string = "https://place-hold.it/300x450?text="+title+"&fontsize=23";
     
     import Card, { Content, Media, MediaContent, PrimaryAction } from "@smui/card";
 
@@ -12,21 +13,24 @@
     // TODO: navigate to detail screen
     function movieClicked() {
       console.log("Movie card clicked!");
+      window.location.href = "/watch/?id=" + movieID;
     }
 </script>
 
 
 
 <div class="card-container">
-  <Card style="width: 300px; height: 450px; background-image: url({coverImageUrl});">
+  <Card style="width: 300px; height: 450px; background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(0,0, 0, 0.4) 97%), url({coverImageUrl}); ">
     <PrimaryAction on:click={movieClicked}>
          <Content style="height: 450px;">
             <div style="color: #fff; position: absolute; bottom: 16px; left: 16px;">
               <h2 class="mdc-typography--headline6" style="margin: 0;">{title}</h2>
-              <h3 class="mdc-typography--subtitle2" style="margin: 0;">And a subtitle. {movieID}</h3>
+              <!-- <h3 class="mdc-typography--subtitle2" style="margin: 0;">And a subtitle. {movieID}</h3> -->
             </div>
          </Content>
     </PrimaryAction>
   </Card>
 </div>
+
+
 
