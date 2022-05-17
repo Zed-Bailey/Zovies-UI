4da1592c1e820d117dbdd7aa4a2986e556915830
Zed <emailzed03@gmail.com>
2022-05-17 12:11:21 +1000

    made the download page look nicer
    Re made url input based on https://github.com/hperrin/svelte-material-ui/blob/master/packages/site/src/routes/demo/textfield/_Solo.svelte
    
    added a circular progress bar to show user something's happening

--- zovies/src/routes/download.svelte
+++ zovies/src/routes/download.svelte
@@ -1,82 +1,112 @@
 <script lang="ts">
-    import Textfield from '@smui/textfield';
-    import Icon from '@smui/textfield/icon';
-    import HelperText from '@smui/textfield/helper-text';
-    import Button from '@smui/button/src/Button.svelte';
-    import { Label } from '@smui/button';
-    import { ApiBase, DownloadedMovieIds } from '..//persistent/api';
-    import type { SnackbarComponentDev } from '@smui/snackbar';
-    import Snackbar from '@smui/snackbar';
-    
-    let snackbar: SnackbarComponentDev;
+  import { Label } from '@smui/button';
+  import { ApiBase, DownloadedMovieIds } from '..//persistent/api';
+  import type { SnackbarComponentDev } from '@smui/snackbar';
+  import Snackbar from '@smui/snackbar';
+  import CircularProgress from '@smui/circular-progress';
+  import Paper from '@smui/paper';
+  import { Input } from '@smui/textfield';
+  import Fab from '@smui/fab/dist/';
+  import { Icon } from '@smui/common';
 
-    let url = '';
-    const successLabel = 'Movie has been qeued to download';
-    const errorLabel = 'Failed to find movie';
-    let label = successLabel;
-    
-    async function submitUrl() {
-        let formData = new FormData();
-        formData.append('downloadUrl', url);
-        let response = await fetch(ApiBase + "/download", {
-            body: formData,
-            method: "post"
-        }).then(async response => {
-            label = successLabel;
-            let json = await response.json();
-            $DownloadedMovieIds.concat(json['id']);
-        }).catch(() => label = errorLabel)
-        
-        console.log(response);
 
-        snackbar.open();
-    }
+  let snackbar: SnackbarComponentDev;
+
+  let url = '';
+  const successLabel = 'Movie has been qeued to download';
+  const errorLabel = 'Failed to find movie';
+  let label = successLabel;
+  let submitted = false;
+
+  async function submitUrl() {
+      submitted = true;
+      let formData = new FormData();
+      formData.append('downloadUrl', url);
+      let response = await fetch(ApiBase + "/download", {
+          body: formData,
+          method: "post"
+      }).then(async response => {
+          label = successLabel;
+          let json = await response.json();
+          $DownloadedMovieIds.concat(json['id']);
+      }).catch(() => label = errorLabel)
+      
+      console.log(response);
+      submitted = false;
+      url = '';
+      snackbar.open();
+  }
 </script>
 
 <Snackbar leading bind:this={snackbar}>
     <Label>{label}</Label>
 </Snackbar>
 
-<div style="height: 100%; width:100%;">
-    <Textfield
-        class="shaped-outlined"
-        variant="outlined"
+{#if !submitted}
+  <div class="solo-demo-container solo-container">
+    <Paper class="solo-paper" elevation={6}>
+      <Icon class="material-icons">search</Icon>
+      <Input
         bind:value={url}
-        label="Movie Url"  style="width: 500px;">
-        <HelperText slot="helper">Lookmovie url</HelperText>
-    </Textfield>
-    
-    <Button variant="outlined" on:click={submitUrl} style="margin-left: 175px;">
-        <Icon class="material-icons">upload</Icon>
-        <Label>Download</Label>
-    </Button>
-</div>
+        placeholder="Search"
+        class="solo-input"
+      />
+    </Paper>
+    <Fab
+      on:click={submitUrl}
+      disabled={url === ''}
+      color="primary"
+      mini
+      class="solo-fab"
+    >
+      <Icon class="material-icons">arrow_forward</Icon>
+    </Fab>
+  </div>
+{:else}
+  <div class="solo-container">
+    <CircularProgress
+      class="my-four-colors"
+      style="height: 60px; width: 60px;"
+      indeterminate
+      fourColor
+    />
+  </div>
+{/if}
+
 
    
-  <style>
-    *
-      :global(.shaped-outlined
-        .mdc-notched-outline
-        .mdc-notched-outline__leading) {
-      border-radius: 28px 0 0 28px;
-      width: 28px;
-    }
-    *
-      :global(.shaped-outlined
-        .mdc-notched-outline
-        .mdc-notched-outline__trailing) {
-      border-radius: 0 28px 28px 0;
-    }
-    * :global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__notch) {
-      max-width: calc(100% - 28px * 2);
-    }
-    *
-      :global(.shaped-outlined.mdc-text-field--with-leading-icon:not(.mdc-text-field--label-floating)
-        .mdc-floating-label) {
-      left: 16px;
-    }
-    * :global(.shaped-outlined + .mdc-text-field-helper-line) {
-      padding-left: 32px;
-      padding-right: 28px;
-    }
-  </style>
+<style>
+  .solo-demo-container {
+    padding: 36px 18px;
+  }
+ 
+  .solo-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  * :global(.solo-paper) {
+    display: flex;
+    align-items: center;
+    flex-grow: 1;
+    max-width: 600px;
+    margin: 0 12px;
+    padding: 0 12px;
+    height: 48px;
+  }
+  * :global(.solo-paper > *) {
+    display: inline-block;
+    margin: 0 12px;
+  }
+  * :global(.solo-input) {
+    flex-grow: 1;
+    color: var(--mdc-theme-on-surface, #f8f8f8);
+  }
+  * :global(.solo-input::placeholder) {
+    color: var(--mdc-theme-on-surface, #f8f8f8);
+    opacity: 0.6;
+  }
+  * :global(.solo-fab) {
+    flex-shrink: 0;
+  }
+</style>
